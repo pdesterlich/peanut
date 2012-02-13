73476230a9de4555b5bef633eaebd664260aad5b
Phelipe de Sterlich <p.desterlich@mooreasoft.net>
2012-02-13 22:13:19 +0100

    corretti errori in helper piwik

--- system/helpers/piwik_helper.php
+++ system/helpers/piwik_helper.php
@@ -21,14 +21,14 @@
 			$result .= "<!-- Piwik -->\n";
 			$result .= "<script type='text/javascript'>\n";
 			$result .= "var pkBaseURL = (('https:' == document.location.protocol) ? 'https://{$sHost}/' : 'http://{$sHost}/');\n";
-			$result .= "document.write(unescape('%3Cscript src='' + pkBaseURL + 'piwik.js' type='text/javascript'%3E%3C/script%3E'));\n";
+			$result .= 'document.write(unescape("%3Cscript src=\'" + pkBaseURL + "piwik.js\' type=\'text/javascript\'%3E%3C/script%3E"));'."\n";
 			$result .= "</script><script type='text/javascript'>\n";
 			$result .= "try {\n";
 			$result .= "var piwikTracker = Piwik.getTracker(pkBaseURL + 'piwik.php', {$iTrackingCode});\n";
 			$result .= "piwikTracker.trackPageView();\n";
 			$result .= "piwikTracker.enableLinkTracking();\n";
 			$result .= "} catch( err ) {}\n";
-			$result .= "</script><noscript><p><img src='http://piwik.mooreasoft.net/piwik.php?idsite=2' style='border:0' alt='' /></p></noscript>\n";
+			$result .= "</script><noscript><p><img src='http://{$sHost}/piwik.php?idsite={$iTrackingCode}' style='border:0' alt='' /></p></noscript>\n";
 			$result .= "<!-- End Piwik Tracking Code -->\n";
 			
 			return $result;
