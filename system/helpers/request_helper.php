a35c34b610daa30699d4739408cc1db1ed5f2cd7
Phelipe de Sterlich <p.desterlich@mooreasoft.net>
2012-03-16 23:35:30 +0100

    helper request
    aggiunto helper request per leggere i parametri dalla richiesta

---
+++ system/helpers/request_helper.php
@@ -0,0 +1,28 @@
+<?php 
+
+	/**
+	 * helper request
+	 * lettura parametri GET / POST
+	 *
+	 * @package peanut
+	 * @author Phelipe de Sterlich
+	 **/
+	class request
+	{
+
+		/**
+		 * funzione read
+		 * legge il valore di un parametro eventualmente presente nella richiesta
+		 *
+		 * @return variant
+		 * @author Phelipe de Sterlich
+		 **/
+		public static function read($valueName, $default = "")
+		{
+			$result = $default;
+			if (isset($_REQUEST[$valueName])) $result = $_REQUEST[$valueName];
+		}
+
+	}
+
+?>
