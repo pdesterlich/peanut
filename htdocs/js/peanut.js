d2c77c80e46ae3f5bb286f87c8f59ce58ef25df1
Phelipe de Sterlich <p.desterlich@mooreasoft.net>
2012-05-07 10:46:18 +0200

    ottimizzazione codice javascript

--- htdocs/js/peanut.js
+++ htdocs/js/peanut.js
@@ -1,33 +1,33 @@
 var peanut = {
 
 	url: function(controller, action, id, params) {
-		result = "";
+		var result = "";
 		if (configUrlShort) {
 			// legge l'url base, se necessario toglie lo slash finale
-			urlBase = (configUrlBase.substring(configUrlBase.length-1) == "/") ? configUrlBase.substring(0, configUrlBase.length-2) : configUrlBase;
+			var urlBase = (configUrlBase.substring(configUrlBase.length-1) === "/") ? configUrlBase.substring(0, configUrlBase.length-2) : configUrlBase;
 			// inizializza il risultato (protocollo + url base)
 			result = configUrlProtocol + "://" + urlBase;
-			if (controller) result += "/" + controller; // se definito, aggiungo il controller
-			if (action) result += "/" + action; // se definito, aggiungo l'action
-			if ((id) && (id != '')) result += "/" + id; // se definito, aggiungo l'id
+			if (controller) { result += "/" + controller; } // se definito, aggiungo il controller
+			if (action) { result += "/" + action; } // se definito, aggiungo l'action
+			if ((id) && (id !== '')) { result += "/" + id; } // se definito, aggiungo l'id
 			if (params) {
-				if (params.substring(0, 1) != '?') {
-					if (params.substring(0, 1) == '&') {
+				if (params.substring(0, 1) !== '?') {
+					if (params.substring(0, 1) === '&') {
 						params = '?' + params.substring(1);
 					} else {
 						params = '?' + params;
 					}
 				}
 			}
-			if (params) result += params; // se definito, aggiungo gli eventuali altri parametri
+			if (params) { result += params; } // se definito, aggiungo gli eventuali altri parametri
 		} else {
 			result = configUrlProtocol + "://" + configUrlBase + "/" + "index.php"; // genero l'url di base con il controller
-			if (controller) result += "?controller=" + controller; // se definito, aggiungo il controller
-			if (action) result += "&action=" + action; // se definito, aggiungo l'action
-			if (id) result += "&id=" + id; // se definito, aggiungo l'id
-			if (params) result += params; // se definito, aggiungo gli eventuali altri parametri
+			if (controller) { result += "?controller=" + controller; } // se definito, aggiungo il controller
+			if (action) { result += "&action=" + action; } // se definito, aggiungo l'action
+			if (id) { result += "&id=" + id; } // se definito, aggiungo l'id
+			if (params) { result += params; } // se definito, aggiungo gli eventuali altri parametri
 		}
 		return result; // ritorno l'url calcolato
 	}
 
-}
+};
