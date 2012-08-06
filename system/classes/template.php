bb030607ed9383a8f9aa80fbd08bd715499e924f
Phelipe de Sterlich <p.desterlich@mooreasoft.net>
2012-08-06 17:54:01 +0200

    gestione template
    modificata funzione caricamento template per gestire directories nidificate nel caso in cui nel nome del controller ci sia un underscore

--- system/classes/template.php
+++ system/classes/template.php
@@ -17,12 +17,28 @@
 			$this->templateName = $templateName;
 		}
 
+		/**
+		 * trova un eventuale percorso alternativo per il template
+		 *
+		 * @return string
+		 * @author Phelipe de Sterlich
+		 **/
+		function _findAlternateName($originalName)
+		{
+			$name = explode(DS, $originalName);
+			if (count($name) > 1) $name[0] = str_replace("_", DS, $name[0]);
+			return implode(DS, $name);
+		}
+
 		function _render($fileName, $vars, $baseDir)
 		{
 			$fileFound = true;
+			$altFileName = $this->_findAlternateName($fileName);
 
 			if (file_exists(APP.DS.$baseDir.DS.$fileName)) {
 				$fullPath = APP.DS.$baseDir.DS.$fileName;
+			} else if (file_exists(APP.DS.$baseDir.DS.$altFileName)) {
+				$fullPath = APP.DS.$baseDir.DS.$altFileName;
 			} else if (file_exists(SYSTEM.DS.$baseDir.DS.$fileName)) {
 				$fullPath = SYSTEM.DS.$baseDir.DS.$fileName;
 			} else {
