c1065172c290155983fc49fde39b214cd2398153
Phelipe de Sterlich <p.desterlich@mooreasoft.net>
2012-08-08 22:32:12 +0200

    aggiunta classe DateTimeItalian
    aggiunta classe DateTimeItalian per gestione date con formattazione in lingua italiana

---
+++ system/classes/date_time_italian.php
@@ -0,0 +1,29 @@
+<?php
+
+	/**
+	 * classe DateTimeItalian
+	 * personalizzazione classe DateTime con lingua in italiano
+	 *
+	 * @package Peanut
+	 * @author Phelipe de Sterlich
+	 **/
+	class DateTimeItalian extends DateTime
+	{
+
+		/**
+		 * ritorna la data in base al formato richiesto, usando la lingua italiana
+		 *
+		 * @return string
+		 * @author Phelipe de Sterlich
+		 **/
+		public function format($format)
+		{
+			$english = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Semptember', 'October', 'November', 'December');
+			$italian = array('Lunedi', 'Martedi', 'Mercoledi', 'Giovedi', 'Venerdi', 'Sabato', 'Domenica', 'Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre');
+
+			return str_replace($english, $italian, parent::format($format));
+		}
+
+	} // END class DateTimeItalian extends DateTime
+
+?>
