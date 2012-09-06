f591d782b804599ab4dee0ed70e4f80690d4b61b
Phelipe de Sterlich <p.desterlich@mooreasoft.net>
2012-09-07 00:48:15 +0200

    gestione configurazione
    aggiunta classe Configure per leggere i parametri di configurazione

---
+++ system/classes/configure.php
@@ -0,0 +1,60 @@
+<?php
+
+	/**
+	 * classe Configure
+	 * lettura configurazione
+	 *
+	 * @package Peanut!
+	 * @author Phelipe de Sterlich
+	 **/
+	class Configure
+	{
+
+		/**
+		 * variabile $_config
+		 *
+		 * @var string
+		 **/
+		protected static $_config = array();
+
+		/**
+		 * funzione read
+		 * legge un valore dalla configurazione
+		 * - $config["nome"] deve essere letto con Configure::read("nome")
+		 * - $config["nome"]["parametro"] deve essere letto con Configure::read("nome.parametro")
+		 *
+		 * @return variant
+		 * @author Phelipe de Sterlich
+		 * -----
+		 * codice originale da CakePHP(tm) : Rapid Development Framework (http://cakephp.org) - parzialmente adattato
+		 **/
+		public static function read($path)
+		{
+			if (empty(self::$_config)) {
+				dump ("lettura configurazione");
+				global $config;
+				self::$_config = $config;
+			}
+
+			$data = self::$_config;
+			if (empty($data) || empty($path)) {
+				return null;
+			}
+			if (is_string($path)) {
+				$parts = explode('.', $path);
+			} else {
+				$parts = $path;
+			}
+			foreach ($parts as $key) {
+				if (is_array($data) && isset($data[$key])) {
+					$data =& $data[$key];
+				} else {
+					return null;
+				}
+
+			}
+			return $data;
+		}
+
+	} // END class Configur
+?>
