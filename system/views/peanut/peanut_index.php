a044068b92c854c2766c551c1e0a2c5c3a1fb219
Phelipe de Sterlich <p.desterlich@mooreasoft.net>
2012-09-14 09:36:12 +0200

    pagina Peanut
    aggiunto link a sorgenti su github

--- system/views/peanut/peanut_index.php
+++ system/views/peanut/peanut_index.php
@@ -4,6 +4,7 @@
 	  <h1>Peanut</h1>
 	  <p>the small php framework &#8482;</p>
 	  <p>a small and simple php framework, developed mainly for internal usage by the folks at <?php echo html::ext_link("moorea software", "http://mooreasoft.ch"); ?>.</p>
+	  <p>get the code @ <a href="https://github.com/mrphelz/peanut">github.com/mrphelz/peanut</a></p>
 	</div>
 
 </div>
