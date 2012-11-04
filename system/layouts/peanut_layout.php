5fcba627bb388468599c1965215896427a764fac
Phelipe de Sterlich <p.desterlich@elcosistemi.it>
2012-11-04 21:25:57 +0100

    aggiornamento framework bootstrap (v2.2.1) e jquery (v1.8.2)

--- system/layouts/peanut_layout.php
+++ system/layouts/peanut_layout.php
@@ -4,7 +4,7 @@
 		<title><?php echo $layoutTitle; ?></title>
 		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen" title="no title" charset="utf-8">
 		<link rel="stylesheet" href="css/peanut.css" type="text/css" media="screen" title="no title" charset="utf-8">
-		<script type="text/javascript" src="/js/jquery-1.8.1.min.js"></script>
+		<script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
 		<script type="text/javascript" src="/js/bootstrap.min.js"></script>
 	</head>
 	<body>
