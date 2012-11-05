e6dd0c714fc0b90f0051cdf432e788a4d44be0d2
Phelipe de Sterlich <p.desterlich@elcosistemi.it>
2012-11-05 01:17:46 +0100

    aggiunta libreria ua-parser
    aggiunta libreria open source ua-parser per processare l'user agent (previsto utilizzo futuro in statistiche accesso)

--- system/controllers/peanut_controller.php
+++ system/controllers/peanut_controller.php
@@ -27,6 +27,27 @@
 			// imposta il nome dell'applicazione per layout e template
 			$this->layout->set("appname", "Peanut!");
 			$this->template->set("appname", "Peanut!");
+
+			/*
+			loadClass("", "ua-parser".DS."UAparser");
+			// UA::get();
+			$ua = UA::parse();
+
+			dump ($ua->browser);         // Chrome (can also use $ua->browser)
+			dump ($ua->browserFull);    // Chrome 16.0.912
+
+			dump ($ua->os);             // Mac OS X
+			dump ($ua->osFull);         // Mac OS X 10.6.8
+
+			// some other generic boolean options
+
+			dump ($ua->isMobile);       // true or false
+			dump ($ua->isMobileDevice); // true or false
+			dump ($ua->isTablet);       // true or false
+			dump ($ua->isSpider);       // true or false
+			dump ($ua->isComputer);     // true or false
+			dump ($ua->isUIWebview);    // true or false, iOS-only
+			*/
 		}
 
 	}
