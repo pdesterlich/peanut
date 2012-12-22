664ce6a30504ae0f1cbb17f9bb728dc5528aedf8
Phelipe de Sterlich <p.desterlich@elcosistemi.it>
2012-12-22 15:14:54 +0100

    aggiornamento framework esterni
    - jQuery v1.8.3
    - Bootstrap v2.2.2
    - Font Awesome v2.0

--- system/layouts/peanut_layout.php
+++ system/layouts/peanut_layout.php
@@ -4,8 +4,10 @@
 		<title><?php echo $layoutTitle; ?></title>
 		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen" title="no title" charset="utf-8">
 		<link rel="stylesheet" href="css/peanut.css" type="text/css" media="screen" title="no title" charset="utf-8">
-		<script type="text/javascript" src="/js/jquery-1.8.2.min.js"></script>
+		<link rel="stylesheet" href="css/font-awesome.css" type="text/css" media="screen" title="no title" charset="utf-8">
+		<script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
 		<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="/js/peanut.js"></script>
 	</head>
 	<body>
 		<?php echo $layoutContent; ?>
--- system/views/peanut/peanut_index.php
+++ system/views/peanut/peanut_index.php
@@ -1,10 +1,16 @@
 <div class="container">
 
 	<div class="hero-unit">
-	  <h1>Peanut</h1>
-	  <p>the small php framework &#8482;</p>
-	  <p>a small and simple php framework, developed mainly for internal usage by the folks at <?php echo html::ext_link("moorea software", "http://mooreasoft.ch"); ?>.</p>
-	  <p>get the code @ <a href="https://github.com/mrphelz/peanut">github.com/mrphelz/peanut</a></p>
+		<h1>Peanut</h1>
+		<p>the small php framework &#8482;</p>
+		<p>a small and simple php framework, developed mainly for internal usage by the folks at <?php echo html::ext_link("moorea software", "http://mooreasoft.ch"); ?>.</p>
+		<p>get the code @ <a href="https://github.com/mrphelz/peanut">github.com/mrphelz/peanut</a></p>
+		<h2>includes</h2>
+		<ul>
+			<li>jQuery v1.8.3</li>
+			<li>Bootstrap v2.2.2</li>
+			<li>Font Awesome v2.0</li>
+		</ul>
 	</div>
 
 </div>
